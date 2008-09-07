f5d4de319a441c1dc16d0a4585f48b8858c8136e
Michal Čihař <nijel@debian.org>
2008-09-07 21:33:13 +0000

    Import 4:3.0.0~rc1-2

--- debian/conf/config.inc.php
+++ debian/conf/config.inc.php
@@ -22,6 +22,42 @@ $i = 0;
 // You can disable a server config entry by setting host to ''.
 $i++;
 
+/* Read configuration from dbconfig-common */
+require('/etc/phpmyadmin/config-db.php');
+
+/* Configure according to dbconfig-common if enabled */
+if (!empty($dbname)) {
+    /* Authentication type */
+    $cfg['Servers'][$i]['auth_type'] = 'cookie';
+    /* Server parameters */
+    if (empty($dbserver)) $dbserver = 'localhost';
+    $cfg['Servers'][$i]['host'] = $dbserver;
+
+    if (!empty($dbport)) {
+        $cfg['Servers'][$i]['connect_type'] = 'tcp';
+        $cfg['Servers'][$i]['port'] = $dbport;
+    }
+    //$cfg['Servers'][$i]['compress'] = false;
+    /* Select mysqli if your server has it */
+    $cfg['Servers'][$i]['extension'] = 'mysqli';
+    /* Optional: User for advanced features */
+    $cfg['Servers'][$i]['controluser'] = $dbuser;
+    $cfg['Servers'][$i]['controlpass'] = $dbpass;
+    /* Optional: Advanced phpMyAdmin features */
+    $cfg['Servers'][$i]['pmadb'] = $dbname;
+    $cfg['Servers'][$i]['bookmarktable'] = 'pma_bookmark';
+    $cfg['Servers'][$i]['relation'] = 'pma_relation';
+    $cfg['Servers'][$i]['table_info'] = 'pma_table_info';
+    $cfg['Servers'][$i]['table_coords'] = 'pma_table_coords';
+    $cfg['Servers'][$i]['pdf_pages'] = 'pma_pdf_pages';
+    $cfg['Servers'][$i]['column_info'] = 'pma_column_info';
+    $cfg['Servers'][$i]['history'] = 'pma_history';
+    $cfg['Servers'][$i]['designer_coords'] = 'pma_designer_coords';
+
+    /* Advance to next server for rest of config */
+    $i++;
+}
+
 /* Authentication type */
 //$cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */

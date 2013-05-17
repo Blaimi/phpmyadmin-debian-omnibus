b65e9fbd2a544e7621094e9e3fdbed99b468d884
Thijs Kinkhorst <thijs@debian.org>
2013-05-17 10:50:57 +0000

    Import 4:4.0.1-2

--- debian/conf/config.inc.php
+++ debian/conf/config.inc.php
@@ -17,17 +17,19 @@
  * (also on the filesystem level).
  */
 
-function check_file_access($path)
-{
-    if (is_readable($path)) {
-        return true;
-    } else {
-        error_log(
-            'phpmyadmin: Failed to load ' . $path
-            . ' Check group www-data has read access and open_basedir restrictions.'
-        );
-        return false;
-    }
+if (!function_exists('check_file_access')) {
+	function check_file_access($path)
+	{
+	    if (is_readable($path)) {
+		return true;
+	    } else {
+		error_log(
+		    'phpmyadmin: Failed to load ' . $path
+		    . ' Check group www-data has read access and open_basedir restrictions.'
+		);
+		return false;
+	    }
+	}
 }
 
 // Load secret generated on postinst

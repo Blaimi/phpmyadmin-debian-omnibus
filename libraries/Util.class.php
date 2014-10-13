be8f8a22dfc4ddf21c9843768dd06dab96c0fda8
Michal Čihař <nijel@debian.org>
2014-10-13 08:57:51 +0200

    Imported Upstream version 4.2.10

--- libraries/Util.class.php
+++ libraries/Util.class.php
@@ -444,6 +444,7 @@ class PMA_Util
     {
         // Fixup for newly used names:
         $link = str_replace('_', '-', strtolower($link));
+        $anchor = str_replace('_', '-', strtolower($anchor));
 
         if (empty($link)) {
             $link = 'index';
@@ -3832,19 +3833,7 @@ class PMA_Util
         // If a database name was provided and user does not have the
         // required global privilege, try database-wise permissions.
         if ($db !== null) {
-            // need to escape wildcards in db and table names, see bug #3566
-            // (wildcard characters appear as being quoted with a backslash
-            //  when querying TABLE_SCHEMA.SCHEMA_PRIVILEGES)
-            $db = str_replace(array('%', '_'), array('\%', '\_'), $db);
-            /*
-             * This is to take into account a wildcard db privilege
-             * so we replace % by .* and _ by . to be able to compare
-             * with REGEXP.
-             *
-             * Also, we need to double the inner % to please sprintf().
-             */
-            $query .= " AND '%s' REGEXP"
-                . " REPLACE(REPLACE(TABLE_SCHEMA, '_', '.'), '%%', '.*')";
+            $query .= " AND '%s' LIKE `TABLE_SCHEMA`";
             $schema_privileges = $GLOBALS['dbi']->fetchValue(
                 sprintf(
                     $query,
